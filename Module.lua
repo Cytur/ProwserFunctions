b16d15c4026ca27db20ce2c16e6b3efe288e943b
cytur <61370167+Cytur@users.noreply.github.com>
2022-11-11 22:19:25 -0500

    added topbar( ) function

--- Module.lua
+++ Module.lua
@@ -8,17 +8,35 @@ function website(name)
 	Page.ScrollBarThickness = 8
 	Page.AutomaticCanvasSize = "Y"
 	Page.CanvasSize = UDim2.fromScale(0,1)
+	Page.Name = "Page"
 	return Page
 end
 
 function ResizeWebsite(size)
    for _,v in pairs(app:GetChildren()) do
-     if v.Name == "Page" then
+     if v.Name == "Page" and v:IsA("ScrollingFrame") then
 	v.CanvasSize = udim
      end
    end
 end
 
 function NewWindow(name,size,logoId)
-  print("NewWindow() is not completed yet!")
+	print("NewWindow() is not completed yet!")
+end
+
+function topbar()
+	local websiteLoaded = false
+	for _,v in pairs(app:GetChildren()) do
+		if v.Name == "Page" and v:IsA("ScrollingFrame") then
+			websiteLoaded = true
+		end
+	end
+	if websiteLoaded == true then
+		local bar = new("Frame",app)
+		bar.Size = UDim2.fromScale(1,0.1)
+		bar.Position = UDim2.fromScale(0,0.2)
+		local page = app.Page
+		page.Position = UDim2.fromScale(0,0.3)
+		return bar
+	end
 end
